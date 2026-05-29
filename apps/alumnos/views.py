82a5d96d1026582346fdbfaa0471da8f6ee5f21b
DiehoOrt <diehoortiz4@gmail.com>
2026-05-29 15:17:54 -0600

    cambios de encabezado y lecensias

--- apps/alumnos/views.py
+++ apps/alumnos/views.py
@@ -8,7 +8,8 @@
 #            Hector Jhosue Ramos, Enzo Howard Rivera,
 #            Diego Josue Ortiz, Alejandra Marisol,
 #            Nathaly Portillo, Roberto Leonel Dominguez
-# Licencia:  MIT License
+# Licencia:  Software Propietario — Todos los derechos reservados.
+#            Uso exclusivo autorizado a UNICAES-CRI bajo contrato.
 # ================================================================
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
